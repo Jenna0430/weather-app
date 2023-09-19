3d70791a4fa67fb6df6a16a37c059080d1a2ae66
Jenna0430 <114163480+Jenna0430@users.noreply.github.com>
2023-09-19 15:37:04 +0100

    removing unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import WeatherSearch from "./components/WeatherSearch"
+import Weather from "./Weather"
 
 const App = () => {
   return (
     <>
      <h1>Weather App</h1>
-    <WeatherSearch/> 
+    <Weather/> 
     <p>
     <a href='https://github.com/Jenna0430/weather-app' rel="noreferrer" target='_blank'>open source code:</a> <em>Jenna</em>
     </p>
